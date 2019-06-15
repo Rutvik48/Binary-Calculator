def045f4980553b347bf0f34a0f7126bc251cd90
Rutvik Patel <rutvik48@gmail.com>
2019-06-15 10:38:52 -0700

    Initial Commit

---
+++ Binary Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binary Calculator
+//
+//  Created by RV on 6/15/19.
+//  Copyright © 2019 RV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
